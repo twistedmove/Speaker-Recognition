e359a872f914e84c03e89bf45bd5d986699f89ea
ptJexio <ml.course.mail@gmail.com>
2019-11-27 00:48:41 +0700

    Added path of dataset

--- speaker_diarization/convert.sh
+++ speaker_diarization/convert.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-for f in $(find /hdd/VoxCeleb2/vox2_dev_dataset/dev/aac/ -name '*.m4a'); do ffmpeg -y -i "$f" -ac 1 -ar 16000 "${f%.m4a}.wav" -loglevel quiet; done
+PATH=$1
+
+for f in $(find ${PATH} -name '*.m4a'); do ffmpeg -y -i "$f" -ac 1 -ar 16000 "${f%.m4a}.wav" -loglevel quiet; done
