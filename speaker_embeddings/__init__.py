3e851d4de5a08137e96d2dda0fd63bf4f79ff38c
ptJexio <ml.course.mail@gmail.com>
2019-12-08 00:45:42 +0700

    the model was renamed

--- speaker_embeddings/__init__.py
+++ speaker_embeddings/__init__.py
@@ -1,4 +1,4 @@
-from .models import SpeakerDiarization
+from .models import SpeakerRecognition
 from .dataset import create_dataloders
 from .losses import CustomCrossEntropyLoss, HardTripletLoss
 from .augmentation import get_training_augmentation, get_valid_augmentation
